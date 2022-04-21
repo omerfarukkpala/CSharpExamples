aa3f5cd38955c912fe482ed8a20db10424033c86
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-21 16:23:34 +0300

    matematiksel ,ilişkisel mantıksal operatörler uygulandı

--- ConOperatorler/Program.cs
+++ ConOperatorler/Program.cs
@@ -6,7 +6,58 @@ namespace ConOperatorler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+         Operatorler
+         * 1-Matematiksel Operatorler
+         *  *,/,+,-
+         * ++,--
+         * %
+         * -= ,+=,*=,/=
+         * 
+         * Not:++ veya -- operatorü değişkenin önünde (solunda) ise değişkeni önce işleme sokar daha sonra 
+         * kullanırız
+         * Not:++ veya -- operatörü değişkenin arkasında (sağında ) ise önce değişkeni kullanıırız sonra işleme sokar.
+         * 
+         */
+
+            int a = 5;
+            a = a + 10;
+            a += 10;
+            a -= 10;
+            a *= 10;
+            a /= 10;
+            int b = 0;
+            b = ++a;
+            b = a++;
+
+            //a = 5;
+            //a = a++ + --a;   ?sonucu nedir
+            //a = 5 + --6;
+            //a = 5 + 5;//a=10 olur.
+
+            /*
+             * 2-ilişkisel operatorler
+             * 
+             * ==  -> Eşit mi 
+             * 
+             * < ->  Küçük mü
+
+             * >  -> Büyük mü 
+             * <=  Küçük veya eşit mi
+             * >= Büyük veya eşit mi
+             *!=  -> eşit değil mi 
+             *!< 
+             *!>
+             *!
+             * 
+             *
+             * 3 -mantıksal operatorler 
+             * &  ve 
+             * | veya
+             * !  değil
+             * 
+             * 
+             */
         }
     }
 }

9068270e3932b3a75af85d3d74e8f11333d7798a
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-21 11:54:35 +0300

    bip sesi çıkartıldı console kapanması önlendi

--- ConGiris/Program.cs
+++ ConGiris/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿//bunlar kütüphanedir.
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConGiris
+namespace ConGiris //namespacede bir kütüphanedir.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Consoleda bip sesi çıkartır.
+            // Console.Beep(500,2000);
+
+            Console.WriteLine("merhaba dünya");//writeline bir alt satıra atar.
+            Console.ReadLine();//consolun kapanmasını önler 
         }
     }
 }

3ff3a8d5cd912017babc4f63e9014a8e1831a4b0
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-22 16:38:12 +0300

    Console Tip Dönüşümleri

--- ConTipDonusumleri/Program.cs
+++ ConTipDonusumleri/Program.cs
@@ -6,7 +6,33 @@ namespace ConTipDonusumleri
     {
         static void Main(string[] args)
         {
-            
+               /*
+             *Tip Dönüşümleri:Bir tipteki değeri başka bir tipe dönüştürmeyi sağlar
+             *
+             * 1-Cast:
+             * 1-1 Implictly Cast:
+             * Kendi Kendine dönüşüm işlemi yapar.
+             * Bizim ayrıca dönüşüm işlemi yapmamıza gerek yoktur.
+             * Not:Kesinlikle bir tip diğer tipin içerisinde barınabiliyorsa gerçekleşir.
+             * 
+             * 1-2:Explicitly cast:
+             * Benzer tipler dönüşüm yapmayı sağlar
+             * Bu kez dönüşüm işlemini biz yaparız
+             * 
+             * 2-Convert ve Parse 
+             * Cast işleminin yetersiz odluğu durumlarda kullanılır
+             * Metinsel bir ifadeyi cast yöntemi ile bir tipe dönüşütüremeyiz.
+             
+             * 2-1.Parse:
+             * Sadece string bir değeri başka bir tipe dönüştürmeyi sağlar
+             * Her  tipin içerisinde parse metodu bulunur
+             * Converte göre daha performanslıdır. fakat daha yeteneksizdir.
+             * 
+             * 2.2 -Convert 
+             * Herhangi bir tipi herhangi bir tipe dönüştürebilir parse'a göre daha yetenekli fakat daha performanssızdır
+             * 
+             * 
+             * */
         }
     }
 }

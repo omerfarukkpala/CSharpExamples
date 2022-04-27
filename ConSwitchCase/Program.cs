8affa837756a2787b74e3cfcf00c72d2a4f7695f
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-27 14:11:33 +0300

    switch case yapısı üzerine çalışıldı

--- ConSwitchCase/Program.cs
+++ ConSwitchCase/Program.cs
@@ -6,6 +6,31 @@ namespace ConSwitchCase
     {
         static void Main(string[] args)
         {
+            /*
+           * Karar Yapıları 
+           * 
+           * 2-Switch case 
+           * 
+           * Yazılışı: 
+           * 
+           * switch(koşul){
+           * 
+           * case 1: şunu yap; break;
+           * case 2:şunu yap ;break;
+           * 
+           * Not:if else yapısından daha yeteneksizdir
+           * 
+           * 
+           * 
+           * 
+           * 
+           * }
+           * 
+           * 
+           * 
+           * 
+           */
+
 
         }
     }

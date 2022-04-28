bf2eb4e0a2c62a13127c6a34f484cbfcd06a83b9
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-28 16:38:43 +0300

    for döngüsü klasörü oluşturuldu

---
+++ ConForDongusu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConForDongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

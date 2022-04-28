ae5213467f45fe4adb015b2bfdf51f520d0e8df0
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-28 14:57:16 +0300

    do while örnek sayı girme denendi

--- ConSayiTahminOyunu/Program.cs
+++ ConSayiTahminOyunu/Program.cs
@@ -11,7 +11,34 @@ namespace ConSayiTahminOyunu
             Random rnd = new Random();//referans tipli değişkenlerdir Ramde bir karşılığı vardır(turkuaz renginde referans tipli değişkenler)
             int tutulan = rnd.Next(10);// 0 ile 10 arasında değer üretir rnd.Next(100,150) 100 ile 150 arasında değer üretir.
 
+            int hak = 5;
 
+            //2.adım:kullanıcının rastgele tutulan sayıyı tahmin edene kadar Console'a sayı girmesini sağlamak
+            int girilen = 0;
+
+            do
+            {
+                if (hak == 0)
+                {
+                    Console.Write("Tahmin Hakkınız bitmiştir ");
+                }
+                else
+                {
+                    Console.Write("{0}.Tahmini  giriniz :", hak);
+                    girilen = Convert.ToInt32(Console.ReadLine());//int ' e convert ettik.
+                    if (girilen < tutulan)
+                    {
+                        Console.WriteLine("Daha büyük bir sayı giriniz");
+                    }
+                    else if (girilen > tutulan)
+                    {
+                        Console.WriteLine("Daha küçük bir sayı giriniz");
+                    }
+                    hak--;
+                }
+
+
+            } while (girilen != tutulan && hak != 0);
 
 
 

b47d1fa4fff5c5e99b49121d17f73335962b8746
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-28 14:23:26 +0300

    ilk adım rasgele sayı üret

--- ConSayiTahminOyunu/Program.cs
+++ ConSayiTahminOyunu/Program.cs
@@ -7,6 +7,14 @@ namespace ConSayiTahminOyunu
         static void Main(string[] args)
         {
             //sayı tahmin oyunu
+            //1.adım :rastgele sayı üretmek
+            Random rnd = new Random();//referans tipli değişkenlerdir Ramde bir karşılığı vardır(turkuaz renginde referans tipli değişkenler)
+            int tutulan = rnd.Next(10);// 0 ile 10 arasında değer üretir rnd.Next(100,150) 100 ile 150 arasında değer üretir.
+
+
+
+
+
         }
     }
 }

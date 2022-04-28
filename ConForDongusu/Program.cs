470bb595db661b6e43dcdfdc9847c496eea3fbf0
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-28 17:13:22 +0300

    for dongusu artış uygulandı

--- ConForDongusu/Program.cs
+++ ConForDongusu/Program.cs
@@ -12,6 +12,27 @@ namespace ConForDongusu
             * 
             * 
             */
+
+
+            int deger = 15;
+            for (int sayi = 0; sayi <= 100 && deger == 15; sayi++)
+            {
+
+                Console.WriteLine(sayi);
+
+            }
+
+
+            Console.Clear();
+
+
+
+
+            //Kullanıcının girmiş olduğu sayının faktöriyelini hesaplayan uygulamayı yazınız.
+
+            //1.adım:Kullanıcının sayı girmesini sağlamak ve girilen sayıyı bir değişkende tutmak
+            Console.Write("Faktöriyeli hesaplanacak sayıyı giriniz: ");
+            int faktor = Convert.ToInt32(Console.ReadLine());
         }
     }
 }

96f7b2f3f6e6b6bbad669eea0b7571e9e8b15261
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-28 17:17:58 +0300

    sayının faktöriyelini hesaplama uygulaması yapıldı

--- ConForDongusu/Program.cs
+++ ConForDongusu/Program.cs
@@ -33,6 +33,31 @@ namespace ConForDongusu
             //1.adım:Kullanıcının sayı girmesini sağlamak ve girilen sayıyı bir değişkende tutmak
             Console.Write("Faktöriyeli hesaplanacak sayıyı giriniz: ");
             int faktor = Convert.ToInt32(Console.ReadLine());
+
+            //2.adım:Sayının Faktöriyelinin Hesaplanması
+            if (faktor == 0)
+            {
+                Console.Write("0 sayısının faktöriyeli 1 dir.");
+            }
+            else
+            {
+                int sonuc = 1;
+                for (int i = faktor; i > 0; i--)
+                {
+                    if (i == 3)
+                    {
+                        break;//direk döngüden çıkar (continue deseydik bu aşamayı geçip diğer aşamadan devam ederdi.)
+                    }
+                    Console.Write("{0}*", i);
+                    sonuc *= i;
+                }
+                Console.Write("\n {0} sayısının Faktöriyeli: {1}", faktor, sonuc);
+
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }

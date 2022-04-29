8120fd82c97f99b6658a9de0366a0c4587678dcc
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-29 14:27:51 +0300

    girilen adet kadar isim girilmeli girilen isimler arasından arama yapılmalı

--- ConDiziOrnegi/Program.cs
+++ ConDiziOrnegi/Program.cs
@@ -6,7 +6,8 @@ namespace ConDiziOrnegi
     {
         static void Main(string[] args)
         {
-            
+            //Girilen adet kadar isim girdirilmeli ve girilen isimler arasından arama yapılmalı
+
         }
     }
 }

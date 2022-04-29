d949c629daf7a73f1ec2d35c98ba1f564dfd28bf
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-29 09:58:05 +0300

    dizi tanımlandı

--- ConDiziler/Program.cs
+++ ConDiziler/Program.cs
@@ -10,6 +10,15 @@ namespace ConDiziler
              * aynı iş ve aynı amaç için içerisinde birden fazla deger barındırmayı sağlayan yapılardır.
              * -Dizileri tanımlarken ne kadar yer kaplayacakları yani içerisinde kaç tane eleman barındıracakları belli olsun diye tanımlama
              * sırasıda adet verilir.
+             * * Her tipin dizisi olabilir
+             * Herhangi bir tipi dizi haline getirmek için tipin sonuna "[]" konulur.
+             * Faydası:Aynı isim altında bir tipe ait birden fazla değer tutubilir.
+             * 
+             * Dizilerde numaralandırma index diye adlandırılır
+             * ve 0 dan başlar.Yani 1.sıradaki elemanın index numarası 0 dır.
+             * 
+             * Tanımlama Şekli:
+             * string[] isimler=new string[10]
              */
 
 

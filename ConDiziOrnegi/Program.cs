a1b6049c0aa0fdee406128f9f5361ef580363774
omerfarukkpala <omerfarukkpala@gmail.com>
2022-04-29 15:20:55 +0300

    adım adım isim girişi sağlanması

--- ConDiziOrnegi/Program.cs
+++ ConDiziOrnegi/Program.cs
@@ -8,6 +8,25 @@ namespace ConDiziOrnegi
         {
             //Girilen adet kadar isim girdirilmeli ve girilen isimler arasından arama yapılmalı
 
+
+
+            //1.adım:Kullanıcının kaç tane isim giriceği bilgisini almak
+            Console.Write("Kaç isim girilecek");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+            //2.adım Girilen adet boyutunda dizi tanımlanmalı
+            string[] isimler = new string[adet];
+
+            //3.adım belirtilen adet kadar kullanıcının isim girmesini sağlamak ve girilen isimleri sıra ile diziye atamak
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                Console.Write("{0}.İsmi giriniz: ", i + 1);
+                isimler[i] = Console.ReadLine();//diziye değerleri atadık.
+
+            }
+
+
+
         }
     }
 }

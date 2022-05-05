19483692bfe07d93e16aaae71c0d49f3acc2047c
omerfarukkpala <omerfarukkpala@gmail.com>
2022-05-05 17:28:34 +0300

    oyuncu ismi girişinin sağlanması

--- ConTakimOlusturma/Program.cs
+++ ConTakimOlusturma/Program.cs
@@ -10,6 +10,29 @@ namespace ConTakimOlusturma
 
             //1.adım:Kullanıcıya kaç isim gireceği sorulup girilen adet boyutunda dizi tanımlanacak.
             //  girilen adet çift sayı olmak zorundadır.
+            int adet = 0;
+            string[] isimler = new string[0];
+            do
+            {
+                Console.Write("Kaç isim girilecek: ");
+                adet = Convert.ToInt32(Console.ReadLine());
+                if (adet % 2 == 0)//çift sayı ise
+                {
+                    isimler = new string[adet];//isimler dizisini yeniden boyutlandır.
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen çift sayı giriniz");
+                }
+
+
+            } while (adet % 2 != 0);
+
+            //2.adım:kullanıcının belirtmiş olduğu adet kadar oyuncu ismi girmesini sağlamak ve bu isimleri diziye atmak
+            //ancak bir isim dizidee bir kez bulunabilmelidir.
+
+
+
         }
     }
 }

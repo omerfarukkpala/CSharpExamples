b46feed799f6e1434a726849bb1c79fc97e5a553
omerfarukkpala <omerfarukkpala@gmail.com>
2022-05-05 14:27:21 +0300

    takım oluşturma adımı gerçekleştirildi

--- ConTakimOlusturma/Program.cs
+++ ConTakimOlusturma/Program.cs
@@ -6,7 +6,10 @@ namespace ConTakimOlusturma
     {
         static void Main(string[] args)
         {
-            
+            //Takım Oyunu
+
+            //1.adım:Kullanıcıya kaç isim gireceği sorulup girilen adet boyutunda dizi tanımlanacak.
+            //  girilen adet çift sayı olmak zorundadır.
         }
     }
 }

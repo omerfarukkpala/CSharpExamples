068838ec31f24cc3f161f21ec0e958485f6f60db
omerfarukkpala <omerfarukkpala@gmail.com>
2022-05-05 12:00:15 +0300

    foreach döngüsünün tanımı yapıldı

--- ConForeach/Program.cs
+++ ConForeach/Program.cs
@@ -6,7 +6,13 @@ namespace ConForeach
     {
         static void Main(string[] args)
         {
-           
+            /*Foreach Döngüsü
+             * 
+             * -üç önemli kuralı vardır.
+             * 1-Sadece ileri gider
+             * 2-Readonlydir.(Sadece okunabilir.Yani koleksiyon içerisindeki elemana değer ataması yapılamaz)
+             * 3-koleksiyon yapısı foreach'in çalışması sırasında değiştirilemez.
+             * */
         }
     }
 }
